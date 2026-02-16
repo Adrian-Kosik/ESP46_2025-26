435e670513f80a86aa51d43b88df2a23a54344b2
Adrian <adrian.kosikowski@student.manchester.ac.uk>
2026-02-16 18:31:28 +0000

    IT WORKED! (well, the basics. long way to go from here)

--- testing_motors.cpp
+++ testing_motors.cpp
@@ -1,18 +1,17 @@
 #include "mbed.h" //https://os.mbed.com/docs/mbed-os/v6.16/debug-test/visual-studio-code.html
 
-// This code is for testing the motor control on the board. It will turn the motor on at full speed and keep it running indefinitely.
-DigitalOut motor_enable(PB_2);   // Enable -> JP1a
-PWMOut motor_pwm_a(PB_1);      // PWM1  -> JP1a
-DigitalOut motor_dir_a(PB_14);      // DIR   -> JP1a
-DigitalOut motor_pwm_b(PB_15);      // PWM2  -> JP1a
-DigitalOut motor_dir_b(PB_13);      // DIR   -> JP1a
-DigitalOut motor_bipolar_a(PC_8);     // Bipolar -> JP1a
-DigitalOut motor_bipolar_b(PC_6);     // Bipolar -> JP1a
+// This code is for testing the motor control on the board. Motor A is the left one, B is right
+DigitalOut motor_enable(PB_2);          // Enable motors to run
+PwmOut motor_pwm_a(PB_1);               // PWM for motors
+PwmOut motor_pwm_b(PB_15);              
+DigitalOut motor_dir_a(PB_14);          // Direction of movement
+DigitalOut motor_dir_b(PB_13);          
+DigitalOut motor_bipolar_a(PC_8);       // Bipolar or Unipolar mode
+DigitalOut motor_bipolar_b(PC_6);       
 
 int main() {
     // Start with everything off
     motor_enable = 0;
-
 
     wait(1.0);   // 1 second delay
 
@@ -20,12 +19,14 @@ int main() {
     motor_enable = 1;
     motor_pwm_a.period_us(100); //Each pulse lasts 0.1ms
     motor_pwm_b.period_us(100); //Each pulse lasts 0.1ms
+
     //Set the motor to run in unipolar mode
     motor_bipolar_a = 0;
     motor_bipolar_b = 0;
+    
     //Set the directions to go forward
-    motor_dir_a = 1;
-    motor_dir_b = 1;
+    motor_dir_a = 1; //goes back
+    motor_dir_b = 0; //goes forward
     
     while (true) {
         //Make it run for ever
@@ -38,5 +39,5 @@ int main() {
         motor_pwm_b.pulsewidth_us(25); 
 
         wait(2);
-    }
+    };
 }
