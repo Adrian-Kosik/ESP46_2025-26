e1657887e34fa1b816d3bcb8e26b8a4cbb8f69a8
Adrian <adrian.kosikowski@student.manchester.ac.uk>
2026-03-08 20:05:33 +0000

    BLE serial in 2 directions

--- bluetooth.cpp
+++ bluetooth.cpp
@@ -12,9 +12,11 @@ int index = 0;
 int main() {
     hm10.baud(9600);
     while (!hm10.writeable()) { }  // wait until HM10 is ready
+    
     while (1) {
         if (hm10.readable()) {
             char s = hm10.getc();
+
             if (s == '1') {
                 LED = 1;
                 continue;
@@ -23,12 +25,20 @@ int main() {
                 LED = 0;
                 continue;
             }
+
+            if (s == '2') {
+                hm10.printf("Recieved number 2!\n");
+                continue;
+            }
+
             if (s == '\n' || s == '\r') {
                 if (index > 0) {
                     box[index] = '\0';
                     lcd.cls();
                     lcd.locate(1,1);
                     lcd.printf("%s", box);
+
+                    hm10.printf("You sent: %s\n", box);
                     index = 0;
                 }
                 continue;
